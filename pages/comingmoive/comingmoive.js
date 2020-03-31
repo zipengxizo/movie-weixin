48a644f444c18c3eb5e6c96dbd9f0889d4b8ebb4
肖子朋 <929822043@qq.com>
2020-03-31 20:23:30 +0800

    tabbar
    add tabbar

---
+++ pages/comingmoive/comingmoive.js
@@ -0,0 +1,27 @@
+
+
+import api from "../../utils/http.js";
+const app = getApp();
+Page({
+  data:{
+    movieList : []
+  },
+  onLoad : function(){
+    wx.showLoading({
+      title: '加载中',
+    });
+    let that = this;
+    api.get("movieComingList?cityId=10").then((res)=>{
+      wx.hideLoading();
+
+      let movieList = res.data.data.comingList;
+      let changeMovieList = movieList.map((item)=>{
+        item.img = item.img.replace(/w\.h/,'128.180');
+        return item;
+      });
+      that.setData({movieList : changeMovieList});
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+});
